744df461b8447041d4a4841f845e73a47ea2282a
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-07-25 21:35:12 +0000

    rendered products added to bag in bag.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from product.models import Product
+from products.models import Product
 
 
 def bag_contents(request):
@@ -28,11 +28,15 @@ def bag_contents(request):
         delivery = 0
         free_delivery_delta = 0
 
+    grand_total = delivery + total
+
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
