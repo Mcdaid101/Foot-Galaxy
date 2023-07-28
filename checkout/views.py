a6a66a62593672842f49dea2d97bcaa5d59faba9
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-07-28 21:21:15 +0000

    added credit card payment div from stripe to the checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -10,10 +14,14 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NO5HcEFg1TajCpJ8TDgawPG2iD4aVONURDFTKiO0FvU6XHaXEb4vKS5v7oejKnyXiOr8Msn6uoJ8Vc6aYTScVZV00bvVEhq8z',
+         'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
