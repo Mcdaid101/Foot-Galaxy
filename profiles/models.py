9f9187716413296e4fcf247d8a31782f476c8cdd
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-07-29 19:34:12 +0000

    added crispy forms to allauth templates and added login functionality for users to access their user profile

--- profiles/models.py
+++ profiles/models.py
@@ -8,12 +8,13 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    user profile for delivery info and order history
+    A user profile model for maintaining default
+    delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=15, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
@@ -22,13 +23,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-    
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    create or update user profile
+    Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
-        # existing users just save the profile
-        instance.userprofile.save()
+    # Existing users: just save the profile
+    instance.userprofile.save()
