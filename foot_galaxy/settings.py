bc0a84c1713ff9b47e40a6d6d5e74db34be2b4bc
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-08-01 16:56:55 +0000

    deploying to heroku

--- foot_galaxy/settings.py
+++ foot_galaxy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-0=d&)o026%%ac-!#962(jz=-q(=^o=j$k*=8@mz9o^vmh9fa_3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mcdaid101-footgalaxy-yuozmm61bed.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['localhost', 'https://foot-galaxy-e4d24e6240cb.herokuapp.com/']
 
 
 # Application definition
@@ -136,12 +137,18 @@ WSGI_APPLICATION = 'foot_galaxy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATBASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABSE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
