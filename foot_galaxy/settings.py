16f287201f7e84c2828b363dbea017287d420e0c
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-08-01 17:45:49 +0000

    removed secret key and debug

--- foot_galaxy/settings.py
+++ foot_galaxy/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0=d&)o026%%ac-!#962(jz=-q(=^o=j$k*=8@mz9o^vmh9fa_3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'foot-galaxy-e4d24e6240cb.herokuapp.com']
 
