901d238b311752c3dc025a1cd909b4acd83168ac
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-08-01 17:51:11 +0000

    deployment part 3

--- foot_galaxy/settings.py
+++ foot_galaxy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'foot-galaxy-e4d24e6240cb.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'https://foot-galaxy-e4d24e6240cb.herokuapp.com/']
 
 
 # Application definition
