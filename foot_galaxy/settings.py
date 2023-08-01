f3480b54952799d8069c93c09071021da6cef328
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-08-01 18:48:03 +0000

    deployment issue

--- foot_galaxy/settings.py
+++ foot_galaxy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'foot-galaxy-e4d24e6240cb.herokuapp.com', '8000-mcdaid101-footgalaxy-yuozmm61bed.ws-eu102.gitpod.io']
 
