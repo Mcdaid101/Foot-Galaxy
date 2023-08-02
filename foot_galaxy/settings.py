b2f0975cc71f40cfbb0035b5664d71ca151fd9d1
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-08-02 23:23:22 +0000

    added robots.txt file

--- foot_galaxy/settings.py
+++ foot_galaxy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'foot-galaxy-e4d24e6240cb.herokuapp.com', '8000-mcdaid101-footgalaxy-yuozmm61bed.ws-eu102.gitpod.io']
 
